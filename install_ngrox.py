53bed9768e23893e0c5b0951af0dddd3a5eeab6b
willikscool2 <85990748+willikscool2@users.noreply.github.com>
2021-06-27 17:40:35 +0700

    Update install_ngrox.py

--- install_ngrox.py
+++ install_ngrox.py
@@ -1,4 +1,3 @@
-import apt, apt.debfile
 import pathlib, stat, shutil, urllib.request, subprocess, getpass, time, tempfile
 import secrets, json, re
 import IPython.utils.io
@@ -8,75 +7,6 @@ import os
 from IPython.display import clear_output
 
 
-
-class _NoteProgress(apt.progress.base.InstallProgress, apt.progress.base.AcquireProgress, apt.progress.base.OpProgress):
-  def __init__(self):
-    apt.progress.base.InstallProgress.__init__(self)
-    self._label = ipywidgets.Label()
-    display(self._label)
-    self._float_progress = ipywidgets.FloatProgress(min = 0.0, max = 1.0, layout = {'border':'1px solid #118800'})
-    display(self._float_progress)
-
-  def close(self):
-    self._float_progress.close()
-    self._label.close()
-
-  def fetch(self, item):
-    self._label.value = "fetch: " + item.shortdesc
-
-  def pulse(self, owner):
-    self._float_progress.value = self.current_items / self.total_items
-    return True
-
-  def status_change(self, pkg, percent, status):
-    self._label.value = "%s: %s" % (pkg, status)
-    self._float_progress.value = percent / 100.0
-
-  def update(self, percent=None):
-    self._float_progress.value = self.percent / 100.0
-    self._label.value = self.op + ": " + self.subop
-
-  def done(self, item=None):
-    pass
-
-class _MyApt:
-  def __init__(self):
-    self._progress = _NoteProgress()
-    self._cache = apt.Cache(self._progress)
-
-  def close(self):
-    self._cache.close()
-    self._cache = None
-    self._progress.close()
-    self._progress = None
-
-  def update_upgrade(self):
-    self._cache.update()
-    self._cache.open(None)
-    self._cache.upgrade()
-
-  def commit(self):
-    self._cache.commit(self._progress, self._progress)
-    self._cache.clear()
-
-  def installPkg(self, *args):
-    for name in args:
-      pkg = self._cache[name]
-      if pkg.is_installed:
-        print(f"{name} is already installed")
-      else:
-        pkg.mark_install()
-
-  def installDebPackage(self, name):
-    apt.debfile.DebPackage(name, self._cache).install()
-
-  def deleteInstalledPkg(self, *args):
-    for pkg in self._cache:
-      if pkg.is_installed:
-        for name in args:
-          if pkg.name.startswith(name):
-            #print(f"Delete {pkg.name}")
-            pkg.mark_delete()
 
 def _download(url, path):
   try:
@@ -117,10 +47,7 @@ def _setupssh():
   clear_output()
 
   #SSH Dropbear
-  my_apt = _MyApt()
-  my_apt.installPkg("dropbear")
-  my_apt.commit()
-  my_apt.close()
+  os.system("sudo apt-get install -y dropbear")
 
   #Set Dropbear
   f = open("../etc/default/dropbear", "w")
